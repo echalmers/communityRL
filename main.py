8ffda8949a523c964720a035a1391c138b18acd8
echalmers <echalmers@mtroyal.ca>
2023-02-02 08:41:27 -0700

    basic network

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from network import Network, TaskRewardNetwork
 import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
 import random
 import pandas as pd
+from multiprocessing import Pool
 
 
 data = {
@@ -20,60 +22,78 @@ data = {
 #     (1, ): [0],
 # }
 
-net = Network(n_hidden=4, n_outputs=len(list(data.values())[0]), starting_energy=100)
-# net = TaskRewardNetwork(n_hidden=4, n_outputs=len(list(data.values())[0]), starting_energy=100)
+def run_experiment(net):
+    print(f'running with {net.__class__.__name__}...')
 
-h_input = []
-h_hidden = []
-h_output = []
-h_energy = []
-task_success = []
+    results = []
+    steps = 10000
+    e_schedule = np.linspace(0.5, 0.0, steps) ** 2
 
-steps = 10000
-e_schedule = np.linspace(0.5, 0.0, steps) ** 2
-
-input = random.choice(list(data))
-for step in range(steps):
-    # if step % 5 == 0:
     input = random.choice(list(data))
-    net.set_epsilon(e_schedule[step])
+    for step in range(steps):
+        # if step % 5 == 0:
+        input = random.choice(list(data))
+        net.set_epsilon(e_schedule[step])
 
-    output = net.act([input])
-    if all(np.array(output) == np.array(data[input])):
-        net.add_energy(net.n_neurons * 1.5)
-        task_success.append(1)
-    else:
-        net.add_energy(0)
-        task_success.append(0)
+        output = net.act([input])
+        if all(np.array(output) == np.array(data[input])):
+            net.add_energy(net.n_neurons * 1.5)
+            task_success = 1
+        else:
+            net.add_energy(0)
+            task_success = 0
 
-    h_input.append(input)
-    h_hidden.append(net.hidden_state[:-1])
-    h_output.append(output)
-    h_energy.append(net.energy_pool)
+        results.append({
+            'net': net.__class__.__name__,
+            'step': step,
+            # 'input': input,
+            # 'hidden': net.hidden_state,
+            # 'output': output,
+            'success': task_success
+        })
+    results = pd.DataFrame(results)
+    results['success'] = results['success'].rolling(100).mean()
+    return results
 
 
-f = plt.subplot(5, 1, 1)
-plt.plot(pd.Series(task_success).rolling(50).mean())
+if __name__ == '__main__':
 
-plt.subplot(5, 1, 2, sharex=f)
-plt.plot(h_energy)
-zeros = np.arange(0,len(h_energy))[np.array(h_energy) == 0]
-plt.scatter(zeros, np.zeros(len(zeros)), c='red', marker='x')
-plt.grid()
+    nets = [Network(n_hidden=4, n_outputs=len(list(data.values())[0])) for _ in range(3)]
+    nets += [TaskRewardNetwork(n_hidden=4, n_outputs=len(list(data.values())[0])) for _ in range(3)]
 
-plt.subplot(5, 1, 3, sharex=f)
-lines = list(zip(*h_input))
-for i in range(len(lines)):
-    plt.plot(np.array(lines[i]) + i)
+    pool = Pool(6)
+    df = pool.map(run_experiment, nets)
+    pool.close()
 
-plt.subplot(5, 1, 4, sharex=f)
-lines = list(zip(*h_hidden))
-for i in range(len(lines)):
-    plt.plot(np.array(lines[i]) + i)
+    print('joining...')
+    df = pd.concat(df, ignore_index=True)
 
-plt.subplot(5, 1, 5, sharex=f)
-lines = list(zip(*h_output))
-for i in range(len(lines)):
-    plt.plot(np.array(lines[i]) + i)
+    print('plotting...')
+    sns.lineplot(df, x='step', y='success', hue='net', errorbar='sd', n_boot=1)
+    plt.show()
 
-plt.show()
+# f = plt.subplot(5, 1, 1)
+# plt.plot(pd.Series(task_success).rolling(50).mean())
+#
+# plt.subplot(5, 1, 2, sharex=f)
+# plt.plot(h_energy)
+# zeros = np.arange(0,len(h_energy))[np.array(h_energy) == 0]
+# plt.scatter(zeros, np.zeros(len(zeros)), c='red', marker='x')
+# plt.grid()
+#
+# plt.subplot(5, 1, 3, sharex=f)
+# lines = list(zip(*h_input))
+# for i in range(len(lines)):
+#     plt.plot(np.array(lines[i]) + i)
+#
+# plt.subplot(5, 1, 4, sharex=f)
+# lines = list(zip(*h_hidden))
+# for i in range(len(lines)):
+#     plt.plot(np.array(lines[i]) + i)
+#
+# plt.subplot(5, 1, 5, sharex=f)
+# lines = list(zip(*h_output))
+# for i in range(len(lines)):
+#     plt.plot(np.array(lines[i]) + i)
+#
+# plt.show()
--- network.py
+++ network.py
@@ -5,7 +5,7 @@ import numpy as np
 
 class Network:
 
-    def __init__(self, n_hidden, n_outputs, starting_energy):
+    def __init__(self, n_hidden, n_outputs):
         self.hidden_layer = [TabularLearner([0, 1], default_value=100, alpha=0.1, gamma=0.95, epsilon=0.1)
                              for _ in range(n_hidden)]
         self.output_layer = [TabularLearner([0, 1], default_value=100, alpha=0.1, gamma=0.95, epsilon=0.1)
@@ -16,10 +16,10 @@ class Network:
         self.hidden_state = None
         self.output_state = None
 
-        self.energy_pool = starting_energy
+        self.reward = 0
 
     def add_energy(self, amount):
-        self.energy_pool += amount
+        self.reward = amount
 
     def set_epsilon(self, e):
         for agent in self.hidden_layer:
@@ -27,41 +27,24 @@ class Network:
         for agent in self.output_layer:
             agent.epsilon = e
 
-    @property
-    def energy_state(self):
-        if self.energy_pool <= 0:
-            return 0
-        elif self.energy_pool < 1.5 * self.n_neurons:
-            return 1
-        return 2
-
-    def augment_reward(self, reward, agent):
-        if self.energy_state == 0:
-            return reward - 100
-        return reward
+    def reward_function(self, task_reward, action):
+        return task_reward - action
 
     def act(self, input):
 
         # forward pass
-        energy_state = self.energy_state
-        new_input_state = np.append(input, energy_state)
+        new_input_state = input
 
-        new_hidden_state = np.append(
-            np.array([agent.select_action(new_input_state) for agent in self.hidden_layer]),
-            energy_state
-        )
+        new_hidden_state = np.array([agent.select_action(new_input_state) for agent in self.hidden_layer])
 
         new_output_state = np.array([agent.select_action(new_hidden_state) for agent in self.output_layer])
-
-        # energy cost
-        self.energy_pool = max(0, self.energy_pool - (new_hidden_state.sum() + new_output_state.sum()))
 
         # learning process
         if self.input_state is not None:
             for i in range(len(self.hidden_layer)):
-                self.hidden_layer[i].update(state=self.input_state, action=self.hidden_state[i], reward=self.augment_reward(self.hidden_state[i], self.hidden_layer[i]), new_state=new_input_state)
+                self.hidden_layer[i].update(state=self.input_state, action=self.hidden_state[i], reward=self.reward_function(self.reward, self.hidden_state[i]), new_state=new_input_state)
             for i in range(len(self.output_layer)):
-                self.output_layer[i].update(state=self.hidden_state, action=self.output_state[i], reward=self.augment_reward(self.output_state[i], self.output_layer[i]), new_state=new_hidden_state)
+                self.output_layer[i].update(state=self.hidden_state, action=self.output_state[i], reward=self.reward_function(self.reward, self.output_state[i]), new_state=new_hidden_state)
 
         # reset for next call
         self.input_state = new_input_state
@@ -73,11 +56,5 @@ class Network:
 
 class TaskRewardNetwork(Network):
 
-    reward = 0
-
-    def add_energy(self, amount):
-        self.reward = amount
-        super().add_energy(amount)
-
-    def augment_reward(self, reward, agent):
-        return self.reward
+    def reward_function(self, task_reward, action):
+        return task_reward
